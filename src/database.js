1ccdb55f890ea36847b462f8d4fd6644eaf0720f
SkyHawkB <danielmey626@gmail.com>
2018-07-20 13:13:36 -0400

    some code

---
+++ src/database.js
@@ -0,0 +1,63 @@
+class KeyValueDatabase {
+  constructor(id) {
+    this.id = id;
+    this.path = 'db/' + this.id + '.json';
+
+    this.data = {};
+
+    if(!fs.existsSync(this.path)) {
+      console.log(this);
+      fs.writeFileSync(this.path, '{}');
+    } else {
+      try {
+        const newData = JSON.parse(fs.readFileSync(this.path));
+
+        this.data = newData;
+      } catch(e) {
+        throw new Error('Given path is not empty and its contents are not valid JSON!');
+      }
+    }
+  }
+
+  set(key, value) {
+    this.data[key] = value;
+
+    this.sync();
+  }
+
+  get(key) {
+    return this.data[key];
+  }
+
+  has(key) {
+    return this.data.hasOwnProperty(key);
+  }
+
+  delete(key) {
+    if(this.data.hasOwnProperty(key)) {
+      delete this.data[key]
+    }
+
+    this.sync();
+  }
+
+  clear() {
+    this.data = {};
+
+    this.sync();
+  }
+
+  sync() {
+    try {
+      fs.writeFileSync(this.path, JSON.stringify(this.data, null, 2));
+    } catch(e) {
+      throw new Error('Error synchronizing database to disk!');
+    }
+  }
+
+  getAll() {
+    return this.data;
+  }
+};
+
+module.exports = new KeyValueDatabase('userzones');
--- src/index.js
+++ src/index.js
@@ -1,61 +1,8 @@
-class KeyValueDatabase {
-  constructor(id) {
-    this.id = id;
-    this.path = 'db/' + this.id + '.json';
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+const db = require('./database.js');
 
-    this.data = {};
+fs.readdir('./commands', (err, files) => {
 
-    if(!fs.existsSync(this.path)) {
-      console.log(this);
-      fs.writeFileSync(this.path, '{}');
-    } else {
-      try {
-        const newData = JSON.parse(fs.readFileSync(this.path));
-
-        this.data = newData;
-      } catch(e) {
-        throw new Error('Given path is not empty and its contents are not valid JSON!');
-      }
-    }
-  }
-
-  set(key, value) {
-    this.data[key] = value;
-
-    this.sync();
-  }
-
-  get(key) {
-    return this.data[key];
-  }
-
-  has(key) {
-    return this.data.hasOwnProperty(key);
-  }
-
-  delete(key) {
-    if(this.data.hasOwnProperty(key)) {
-      delete this.data[key]
-    }
-
-    this.sync();
-  }
-
-  clear() {
-    this.data = {};
-
-    this.sync();
-  }
-
-  sync() {
-    try {
-      fs.writeFileSync(this.path, JSON.stringify(this.data, null, 2));
-    } catch(e) {
-      throw new Error('Error synchronizing database to disk!');
-    }
-  }
-
-  getAll() {
-    return this.data;
-  }
-}
+});
