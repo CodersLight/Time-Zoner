343083d7f4bdec2a971fab1ea306d04fd82c975c
SkyHawkB <danielmey626@gmail.com>
2018-07-24 16:54:58 -0400

    added timein command (first draft)

--- src/commands/timein.js
+++ src/commands/timein.js
@@ -1,3 +1,12 @@
+const timeutils = require('../timeutils.js');
+const embedutils = require('../embedutils.js');
+
 module.exports.run = (client, message, config) => {
-	
+	const [,zone] = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const timeIn = timeutils.getTimeIn(zone);
+	if(timeIn !== null) {
+		message.channel.send(embedutils.timeSuccessEmbed(zone, timeIn));
+	} else {
+		message.channel.send(embedutils.timezoneNotFoundEmbed(zone));
+	}
 };
