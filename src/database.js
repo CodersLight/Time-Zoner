0f53fb8cd3367825b5e8f9e7a93e8fb77ce66895
SkyHawkB <danielmey626@gmail.com>
2018-07-24 15:02:42 -0400

    update database file

--- src/database.js
+++ src/database.js
@@ -1,63 +1,65 @@
+const fs = require('fs');
+
 class KeyValueDatabase {
-  constructor(id) {
-    this.id = id;
-    this.path = 'db/' + this.id + '.json';
+	constructor(id) {
+		this.id = id;
+		this.path = 'db/' + this.id + '.json';
 
-    this.data = {};
+		this.data = {};
 
-    if(!fs.existsSync(this.path)) {
-      console.log(this);
-      fs.writeFileSync(this.path, '{}');
-    } else {
-      try {
-        const newData = JSON.parse(fs.readFileSync(this.path));
+		if(!fs.existsSync(this.path)) {
+			console.log(this);
+			fs.writeFileSync(this.path, '{}');
+		} else {
+			try {
+				const newData = JSON.parse(fs.readFileSync(this.path));
 
-        this.data = newData;
-      } catch(e) {
-        throw new Error('Given path is not empty and its contents are not valid JSON!');
-      }
-    }
-  }
+				this.data = newData;
+			} catch(e) {
+				throw new Error('Given path is not empty and its contents are not valid JSON!');
+			}
+		}
+	}
 
-  set(key, value) {
-    this.data[key] = value;
+	set(key, value) {
+		this.data[key] = value;
 
-    this.sync();
-  }
+		this.sync();
+	}
 
-  get(key) {
-    return this.data[key];
-  }
+	get(key) {
+		return this.data[key];
+	}
 
-  has(key) {
-    return this.data.hasOwnProperty(key);
-  }
+	has(key) {
+		return this.data.hasOwnProperty(key);
+	}
 
-  delete(key) {
-    if(this.data.hasOwnProperty(key)) {
-      delete this.data[key]
-    }
+	delete(key) {
+		if(this.data.hasOwnProperty(key)) {
+			delete this.data[key];
+		}
 
-    this.sync();
-  }
+		this.sync();
+	}
 
-  clear() {
-    this.data = {};
+	clear() {
+		this.data = {};
 
-    this.sync();
-  }
+		this.sync();
+	}
 
-  sync() {
-    try {
-      fs.writeFileSync(this.path, JSON.stringify(this.data, null, 2));
-    } catch(e) {
-      throw new Error('Error synchronizing database to disk!');
-    }
-  }
+	sync() {
+		try {
+			fs.writeFileSync(this.path, JSON.stringify(this.data, null, 2));
+		} catch(e) {
+			throw new Error('Error synchronizing database to disk!');
+		}
+	}
 
-  getAll() {
-    return this.data;
-  }
-};
+	getAll() {
+		return this.data;
+	}
+}
 
 module.exports = new KeyValueDatabase('userzones');
