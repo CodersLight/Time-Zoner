8543fdf8436d194c16ade4f8fea1d52c9cad0d78
SkyHawkB <danielmey626@gmail.com>
2018-07-24 15:03:45 -0400

    added Discord framework

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
-const db = require('./database.js');
+const config = require('./config.json');
 
 fs.readdir('./commands', (err, files) => {
-
+	files.forEach((file) => {
+		// Add the commands to the require() cache.
+		// eslint-disable-next-line no-unused-vars
+		const cache = require(`./commands/${file}`);
+	});
 });
+
+client.on('ready', () => {
+	// eslint-disable-next-line no-console
+	console.log('Connected to Discord!');
+});
+
+client.on('message', (message) => {
+	if(message.author.bot || message.content.indexOf(config.prefix) !== 0) return;
+
+	const command = message.content.slice(config.prefix.length).trim().split(/ +/g).shift().toLowerCase();
+
+	if(command.includes('/') || command.includes('\\')) return;
+
+	if(fs.existsSync(`./commands/${command}.js`)) {
+		try {
+			const commandFile = require(`./commands/${command}.js`);
+
+			commandFile.run(client, message, config);
+		} catch(err) {} // eslint-disable-line no-empty
+	}
+});
+
+client.login(require('./auth.js').TOKEN);
