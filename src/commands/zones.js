5593b6cd37682bfe75f44a526e1f9bcef596e082
SkyHawkB <danielmey626@gmail.com>
2018-07-26 00:47:25 -0400

    added zones command

--- src/commands/zones.js
+++ src/commands/zones.js
@@ -1,3 +1,15 @@
-module.exports.run = (client, message, config) => {
+const embedutils = require('../embedutils.js');
+const timeutils = require('../timeutils.js');
 
+module.exports.run = (client, message, config) => {
+  let zones = '';
+  for(const key of Object.keys(timeutils.zones.toJSON())) {
+    zones += key + '\n';
+  }
+
+  message.author.send(zones).then(() => {
+    message.channel.send(embedutils.zoneMessageSuccessEmbed());
+  }).catch((e) => {
+    message.channel.send(embedutils.zoneMessageFailEmbed());
+  });
 };
